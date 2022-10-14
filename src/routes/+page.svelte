d15d23b5ed2559b63a9d90d67bdc70f1942b3bf6
Vishnu V V <ng.pingme@gmail.com>
2022-10-14 13:33:26 +0100

    Create the landing page with styles and bg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,13 @@
-<h1 class="text-3xl font-bold underline">
-  Home Page
-</h1>
+<div class="grid h-screen place-items-center -mt-20">
+  <img class="place-content-center" src="images/logo.png" width="140" height="auto" alt="RightPath Logo">
+
+  <div class="grid place-items-center">
+    <h1 class="text-6xl text-center font-cabinetExtraBold font-bold w-2/3 px-2">
+      Follow your <span class="text-secondary-500 passion-banner">passion</span> to get the job you are passionate about
+    </h1>
+    <p class="text-nuetral-400 font-redHatDisplay">Not sure how to reach your goal? Dont worry, We will help you  figure out!
+      Let us know what job you are passionate about and we will show you the path to get there.
+    </p>
+    <button class="btn btn-primary w-48 h-10 text-sm font-semibold mt-4">Start Your Journey!</button>
+  </div>
+</div>
