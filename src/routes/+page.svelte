fae297aaf6c668f2daec8871edb344d8fe3592b7
Vishnu V V <ng.pingme@gmail.com>
2022-10-15 15:03:29 +0100

    Add a card under the banner area

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
-<div class="flex flex-col h-96 translate-y-1/3 sm:translate-y-1/2 md:translate-y-2/3 flex-1 p-4 md:px-10 lg:px-52 xl:px-80">
-  <h1 class="text-4xl md:text-5xl text-center font-extrabold">
+<div class="flex flex-col h-max mt-28 sm:mt-44 md:mt-36 lg:mt-24 xl:mt-36 text-center md:px-10 lg:px-52 xl:px-80">
+  <h1 class="text-3xl md:text-5xl font-extrabold">
     Follow your
     <span class="text-secondary-500 passion-banner relative">
       <img class="absolute right-20 bottom-10 h-8 md:bottom-16 md:right-28 md:h-10 lg:h-12"
@@ -12,10 +12,15 @@
     </span>
     to get the job you are passionate about
   </h1>
-  <p class="text-nuetral-400 font-redHatDisplay text-center my-8 leading-5">Not sure how to reach your goal? Dont worry,
+  <p class="text-nuetral-400 my-8 leading-5">Not sure how to reach your goal? Dont worry,
     we will
     help you figure out!
     Let us know what job you are passionate about and we will show you the path to get there.
   </p>
   <button class="btn btn-primary w-48 h-10 text-sm mx-auto font-semibold">Start Your Journey!</button>
+</div>
+<div class="flex flex-col justify-around h-48 lg:w-1/2 mx-auto text-center bg-white rounded-md mt-40 p-4">
+  <p class="font-bold text-xl leading-6 mt-2">Dont know what your passion is?</p>
+  <p class="font-normal text-sm text-nuetral-400 max-w-sm mx-auto">Let us know what job you are passionate about and we will show you the path to get there.</p>
+  <a class="text-primary-500 font-bold text-2xl opacity-25 pointer-events-none" href="/">Let's Go!</a>
 </div>
