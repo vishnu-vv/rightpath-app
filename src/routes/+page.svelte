56e0108fe16e2cbf88f5058b5a691816445002c1
Vishnu V V <ng.pingme@gmail.com>
2022-10-15 15:14:13 +0100

    Add margin top for the Questionaire CTA

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,7 +19,7 @@
   </p>
   <button class="btn btn-primary w-48 h-10 text-sm mx-auto font-semibold">Start Your Journey!</button>
 </div>
-<div class="flex flex-col justify-around h-48 lg:w-1/2 mx-auto text-center bg-white rounded-md mt-40 p-4">
+<div class="flex flex-col justify-around h-48 lg:w-1/2 mx-auto text-center bg-white rounded-md mt-56 p-4">
   <p class="font-bold text-xl leading-6 mt-2">Dont know what your passion is?</p>
   <p class="font-normal text-sm text-nuetral-400 max-w-sm mx-auto">Let us know what job you are passionate about and we will show you the path to get there.</p>
   <a class="text-primary-500 font-bold text-2xl opacity-25 pointer-events-none" href="/">Let's Go!</a>
