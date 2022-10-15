189fec1d6072366950fa7cc85ba77999b1e255c6
Vishnu V V <ng.pingme@gmail.com>
2022-10-15 02:33:03 +0100

    Update the styles in the landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,21 @@
-<div class="flex flex-col h-96 translate-y-1/3 flex-1">
-  <h1 class="text-2xl text-center font-extrabold px-8">
-    Follow your <span class="text-secondary-500 passion-banner">passion</span> to get the job you are passionate about
+<div class="flex flex-col h-96 translate-y-1/3 sm:translate-y-1/2 md:translate-y-2/3 flex-1 p-4 md:px-10 lg:px-52 xl:px-80">
+  <h1 class="text-4xl md:text-5xl text-center font-extrabold">
+    Follow your
+    <span class="text-secondary-500 passion-banner relative">
+      <img class="absolute right-20 bottom-10 h-8 md:bottom-16 md:right-28 md:h-10 lg:h-12"
+        srcset="images/passion-arrow-1.png 375w, images/passion-arrow-1-lg.png 1024w"
+        sizes="(max-width: 1023px) 375px, 1024px" src="images/passion-arrow-1.png" alt="Passion Highlight Arrow 1" />
+      passion
+      <img class="absolute right-0 bottom-10 h-10 md:bottom-16 md:h-12 lg:h-14"
+        srcset="images/passion-arrow-2.png 375w, images/passion-arrow-2-lg.png 1024w"
+        sizes="(max-width: 1023px) 375px, 1024px" src="images/passion-arrow-2.png" alt="Passion Highlight Arrow 2" />
+    </span>
+    to get the job you are passionate about
   </h1>
-  <p class="text-nuetral-400 font-redHatDisplay text-center my-8">Not sure how to reach your goal? Dont worry, we will
+  <p class="text-nuetral-400 font-redHatDisplay text-center my-8 leading-5">Not sure how to reach your goal? Dont worry,
+    we will
     help you figure out!
     Let us know what job you are passionate about and we will show you the path to get there.
   </p>
-  <button class="btn btn-primary w-48 h-10 text-sm text-center font-semibold">Start Your Journey!</button>
+  <button class="btn btn-primary w-48 h-10 text-sm mx-auto font-semibold">Start Your Journey!</button>
 </div>
