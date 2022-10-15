a67788fc484329c274e7eeb86ee50a85f44da362
Vishnu V V <ng.pingme@gmail.com>
2022-10-15 01:04:31 +0100

    Update the background image according to the width

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,16 @@
   import "../fonts.css";
 </script>
 
-<slot />
+<header>
+  <nav>
+    <img class="my-8 mx-auto" src="images/logo.png" width="140" height="30" alt="RightPath Logo">
+  </nav>
+</header>
+
+<main class="py-6 px-4 sm:p-6 md:py-10 md:px-8">
+  <slot />
+</main>
+
+<footer>
+  
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,10 @@
-<div class="grid h-screen place-items-center -mt-20">
-  <img class="place-content-center" src="images/logo.png" width="140" height="auto" alt="RightPath Logo">
-
-  <div class="grid place-items-center">
-    <h1 class="text-6xl text-center font-cabinetExtraBold font-bold w-2/3 px-2 -mt-8">
-      Follow your <span class="text-secondary-500 passion-banner">passion</span> to get the job you are passionate about
-    </h1>
-    <p class="text-nuetral-400 font-redHatDisplay w-1/2 text-center my-8">Not sure how to reach your goal? Dont worry, we will help you figure out!
-      Let us know what job you are passionate about and we will show you the path to get there.
-    </p>
-    <button class="btn btn-primary w-48 h-10 text-sm font-semibold mt-4">Start Your Journey!</button>
-  </div>
+<div class="flex flex-col h-96 translate-y-1/3 flex-1">
+  <h1 class="text-2xl text-center font-extrabold px-8">
+    Follow your <span class="text-secondary-500 passion-banner">passion</span> to get the job you are passionate about
+  </h1>
+  <p class="text-nuetral-400 font-redHatDisplay text-center my-8">Not sure how to reach your goal? Dont worry, we will
+    help you figure out!
+    Let us know what job you are passionate about and we will show you the path to get there.
+  </p>
+  <button class="btn btn-primary w-48 h-10 text-sm text-center font-semibold">Start Your Journey!</button>
 </div>
