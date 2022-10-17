bcfa01152dcc4cc8b1199d618c2648b5621937bc
Vishnu V V <ng.pingme@gmail.com>
2022-10-17 22:51:40 +0100

    Extract the Header as another component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import logo from "$lib/images/logo.png";
+</script>
+
+<header class="grid place-content-center">
+  <nav>
+    <a class="inline-block w-auto" href="/">
+      <img class="my-8 mx-auto" src={logo} width="140" height="30" alt="RightPath Logo">
+    </a>
+  </nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,18 +3,14 @@
   import "../app.css";
   import "../fonts.css";
 	import { openGraph, twitter } from "./+layout";
-  import logo from "$lib/images/logo.png";
+  import Header from "$lib/components/Header.svelte";
 </script>
 
 <MetaTags title="Right Path" description="Follow your passion to get the job you are passionate about. Let us know what job you are passionate about and we will show you the path to get there."
   {twitter} {openGraph}
 />
 
-<header>
-  <nav>
-    <img class="my-8 mx-auto" src={logo} width="140" height="30" alt="RightPath Logo">
-  </nav>
-</header>
+<Header />
 
 <main class="py-6 px-4 sm:p-6 md:py-10 md:px-8">
   <slot />
