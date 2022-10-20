d7a119fc7e0132daed92ecca919c9e47f923e393
Vishnu V V <ng.pingme@gmail.com>
2022-10-20 19:33:17 +0100

    Update the style of Profile section

---
+++ src/lib/components/Profile.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { age, currentUser } from "$lib/shared/stores/user";
+  import { selectedPassionNames } from "$lib/shared/stores/passion";
+  import NameIcon from "$lib/images/name-icon.png";
+  import AgeIcon from "$lib/images/age-icon.png";
+  import PassionIcon from "$lib/images/passion-icon.png";
+</script>
+
+<div class="personal-details grid place-content-center font-semibold text-center">
+  <div class="flex justify-center my-1">
+    <span>
+      <img src={NameIcon} alt="Name Icon" height="20" width="20">
+    </span>
+    <span class="ml-2">{$currentUser}</span>
+  </div>
+  <div class="flex justify-center my-1">
+    <span>
+      <img src={AgeIcon} alt="Name Icon" height="20" width="20">
+    </span>
+    <span class="ml-2">{$age} years old</span>
+  </div>
+  <div class="flex justify-center my-1">
+    <span>
+      <img src={PassionIcon} alt="Name Icon" height="20" width="20">
+    </span>
+    <span class="ml-2">{$selectedPassionNames}</span>
+  </div>
+</div>
--- src/routes/journey/course/+page.svelte
+++ src/routes/journey/course/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import personImage from "$lib/images/person1.png";
-  import { age, currentUser } from "$lib/shared/stores/user";
-  import { selectedPassionNames } from "$lib/shared/stores/passion";
+  import { selectedCourse } from "$lib/shared/stores/course";
   import courseImage from "$lib/images/course.png"
 	import Overlay from "$lib/components/Overlay.svelte";
 	import { isOverlayOpen } from "$lib/shared/stores/overlay";
 	import Courses from "$lib/components/Courses.svelte";
+	import Profile from "$lib/components/Profile.svelte";
 </script>
 
 {#if $isOverlayOpen}
@@ -15,33 +15,30 @@
 {/if}
 
 <div class="grid place-content-center mt-20 text-center px-4">
-  <div class="personal-details grid place-content-center font-semibold">
-    <div class="flex">
-      <span>Name: </span>
-      <span>{$currentUser}</span>
-    </div>
-    <div class="flex">
-      <span>Age: </span>
-      <span>{$age}</span>
-    </div>
-    <div class="flex">
-      <span>Passions: </span>
-      <span>{$selectedPassionNames}</span>
-    </div>
-  </div>
+  <Profile />
   <img class="my-4 mx-auto" src={personImage} width="50" height="50" alt="Person">
   <h2 class="font-extrabold">2022</h2>
-
   <div class="max-w-sm bg-white rounded-lg shadow-md mt-4 pt-4">
     <img class="rounded-t-lg w-11/12 mx-auto" src={courseImage} alt="Select Course" />
-    <div class="p-5 pt-0">
-      <h3 class="font-extrabold text-2xl">Select Course</h3>
-      <p class="mb-2 font-normal text-nuetral-400">Select a course from the currated list of courses that we made based on your passion.</p>
-      <button on:click={() => isOverlayOpen.set(true)} class="btn btn-primary w-full md:w-48 h-10 text-sm mx-auto font-semibold mt-8 grid place-content-center">
-        <span class="align-middle flex">
-          Select Course
-        </span>
-      </button>
-    </div>
+    {#if $selectedCourse}
+      <div class="p-5 pt-2">
+        <h3 class="font-extrabold text-2xl">{$selectedCourse.title}</h3>
+        <button on:click={() => isOverlayOpen.set(true)} class="btn btn-primary w-full md:w-48 h-10 text-sm mx-auto font-semibold mt-8 grid place-content-center">
+          <span class="align-middle flex">
+            Change Course
+          </span>
+        </button>
+      </div>
+    {:else}
+      <div class="p-5 pt-2">
+        <h3 class="font-extrabold text-2xl">Select Course</h3>
+        <p class="mb-2 font-normal text-nuetral-400">Select a course from the currated list of courses that we made based on your passion.</p>
+        <button on:click={() => isOverlayOpen.set(true)} class="btn btn-primary w-full md:w-48 h-10 text-sm mx-auto font-semibold mt-8 grid place-content-center">
+          <span class="align-middle flex">
+            Select Course
+          </span>
+        </button>
+      </div>
+    {/if}
   </div>
 </div>
