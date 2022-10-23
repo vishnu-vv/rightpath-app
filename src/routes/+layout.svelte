bc9d714589d1b6ffeb1fade51f3270bde58e64c3
Vishnu V V <ng.pingme@gmail.com>
2022-10-23 05:56:29 +0100

    Update background image according to the layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,21 @@
-<script>
+<script lang="ts">
 	import { MetaTags } from "svelte-meta-tags";
   import "../app.css";
   import "../fonts.css";
 	import { openGraph, twitter } from "./+layout";
   import Header from "$lib/components/Header.svelte";
+	import { page } from "$app/stores";
+	import { onMount } from "svelte";
+
+  onMount(() => {
+    page.subscribe((page) => {
+      if(page?.routeId?.includes('journey')) {
+        document.body.classList.add('journey');
+      } else {
+        document.body.classList.remove('journey');
+      }
+    });
+  })
 </script>
 
 <MetaTags title="Right Path" description="Follow your passion to get the job you are passionate about. Let us know what job you are passionate about and we will show you the path to get there."
