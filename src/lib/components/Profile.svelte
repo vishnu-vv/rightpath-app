cd396cfe1e2ba76b9989099dd67dcda89d2c7524
Vishnu V V <ng.pingme@gmail.com>
2022-10-23 04:01:32 +0100

    Add a reset journey button

--- src/lib/components/Profile.svelte
+++ src/lib/components/Profile.svelte
@@ -1,12 +1,26 @@
 <script>
-  import { age, currentUser } from "$lib/shared/stores/user";
-  import { selectedPassionNames } from "$lib/shared/stores/passion";
+  import { age, currentUser, dateOfBirth } from "$lib/shared/stores/user";
+  import { selectedPassionNames, selectedPassions } from "$lib/shared/stores/passion";
   import NameIcon from "$lib/images/name-icon.png";
   import AgeIcon from "$lib/images/age-icon.png";
   import PassionIcon from "$lib/images/passion-icon.png";
+	import { CourseAPIFilter, courseAPIFilter, selectedCourse, showFilter } from "$lib/shared/stores/course";
+	import { JobAPIFilter, jobAPIFilter, selectedJob, showJobFilter } from "$lib/shared/stores/job";
+
+  function resetAll() {
+    selectedCourse.set(null);
+    selectedPassions.set([]);
+    dateOfBirth.set(null);
+    currentUser.set('');
+    showFilter.set(false);
+    showJobFilter.set(false);
+    selectedJob.set(null);
+    jobAPIFilter.set(new JobAPIFilter());
+    courseAPIFilter.set(new CourseAPIFilter());
+  }
 </script>
 
-<div class="personal-details grid place-content-center font-semibold text-center md:flex md:absolute md:top-4 md:right-6">
+<div class="personal-details grid place-content-center font-semibold text-center items-center md:flex md:absolute md:top-4 md:right-6">
   <div class="flex justify-center my-1 md:m-2">
     <span>
       <img src={NameIcon} alt="Name Icon" height="20" width="20">
@@ -24,5 +38,12 @@
       <img src={PassionIcon} alt="Name Icon" height="20" width="20">
     </span>
     <span class="ml-2">{$selectedPassionNames}</span>
+  </div>
+  <div class="flex justify-center my-1 md:m-2">
+    <a href="/journey/passion">
+      <button on:click={resetAll} class="btn btn-secondary">
+        Restart journey
+      </button>
+    </a>
   </div>
 </div>
--- src/lib/shared/models/index.ts
+++ src/lib/shared/models/index.ts
@@ -1,4 +1,5 @@
 export interface University {
+  id: string;
   name: string;
   location: string;
   imageUrl?: string;
